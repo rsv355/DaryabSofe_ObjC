b8864a117effde9a1bd59f8241ed8582a8a2e0a3
Masum Chauhan <masum@ws-mac-001s-mac-mini.local>
2016-08-15 16:17:50 +0530

    Initial Commit

---
+++ Daryabsofe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Daryabsofe
+//
+//  Created by Masum Chauhan on 15/08/16.
+//  Copyright © 2016 Webmyne Systems Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Daryabsofe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Daryabsofe
+//
+//  Created by Masum Chauhan on 15/08/16.
+//  Copyright © 2016 Webmyne Systems Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
